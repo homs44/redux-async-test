5c3123f073151c0f428fa6b618724e879cc34826
ingyu yoon <a>
2019-06-02 12:42:09 +0900

    handleActions 추가

--- src/store/articleListReducer.js
+++ src/store/articleListReducer.js
@@ -1,31 +1,13 @@
 import axios from 'axios';
+import { createAction, handleActions } from 'redux-actions'
 
 const GET_ARTICLE_LIST_REQUEST = 'GET_ARTICLE_LIST_REQUEST';
 const GET_ARTICLE_LIST_SUCCESS = 'GET_ARTICLE_LIST_SUCCESS';
 const GET_ARTICLE_LIST_FAILED = 'GET_ARTICLE_LIST_FAILED';
 
-function getArticleListRequest() {
-    return {
-        type: GET_ARTICLE_LIST_REQUEST,
-        payload: null,
-    }
-}
-function getArticleListSuccess(data) {
-    return {
-        type: GET_ARTICLE_LIST_SUCCESS,
-        payload: {
-            data: data
-        },
-    }
-}
-function getArticleListFailed(error) {
-    return {
-        type: GET_ARTICLE_LIST_FAILED,
-        payload: {
-            error: error
-        },
-    }
-}
+const getArticleListRequest = createAction(GET_ARTICLE_LIST_REQUEST);
+const getArticleListSuccess = createAction(GET_ARTICLE_LIST_SUCCESS);
+const getArticleListFailed = createAction(GET_ARTICLE_LIST_FAILED);
 
 export function getArticleList() {
     return (dispatch) => {
@@ -34,42 +16,22 @@ export function getArticleList() {
             .then((response) => {
                 return response.data;
             }).then((data) => {
-                dispatch(getArticleListSuccess(data));
+                dispatch(getArticleListSuccess({ data: data }));
             }).catch((error) => {
                 console.log(error);
-                dispatch(getArticleListFailed(new Error('get article list failed')))
+                dispatch(getArticleListFailed({ error: new Error('get article list failed') }))
             })
     }
 }
-
 
 //delete
 const DELETE_ARTICLE_REQUEST = 'DELETE_ARTICLE_REQUEST';
 const DELETE_ARTICLE_SUCCESS = 'DELETE_ARTICLE_SUCCESS';
 const DELETE_ARTICLE_FAILED = 'DELETE_ARTICLE_FAILED';
 
-function deleteArticleRequest() {
-    return {
-        type: DELETE_ARTICLE_REQUEST,
-        payload: null,
-    }
-}
-function deleteArticleSuccess(deletedId) {
-    return {
-        type: DELETE_ARTICLE_SUCCESS,
-        payload: {
-            deletedId: deletedId
-        },
-    }
-}
-function deleteArticleFailed(error) {
-    return {
-        type: DELETE_ARTICLE_FAILED,
-        payload: {
-            error: error
-        },
-    }
-}
+const deleteArticleRequest = createAction(DELETE_ARTICLE_REQUEST);
+const deleteArticleSuccess = createAction(DELETE_ARTICLE_SUCCESS);
+const deleteArticleFailed = createAction(DELETE_ARTICLE_FAILED);
 
 export function deleteArticle(id) {
     return (dispatch) => {
@@ -84,58 +46,106 @@ export function deleteArticle(id) {
             })
     }
 }
-//update
 
-//add
 
-export default function articleListReducer(state = {
+const initialState = {
     isLoading: false,
     isSuccess: false,
     isFailed: false,
     list: [],
-    erorr: null,
-}, { type, payload }) {
-    switch (type) {
-        case GET_ARTICLE_LIST_REQUEST:
-            return Object.assign({}, state, {
-                isLoading: true,
-                isSuccess: false,
-                isFailed: false,
-            })
-        case GET_ARTICLE_LIST_SUCCESS:
-            return Object.assign({}, state, {
-                isLoading: false,
-                isSuccess: true,
-                isFailed: false,
-                list: [...payload.data]
-            })
-        case GET_ARTICLE_LIST_FAILED:
-            return Object.assign({}, state, {
-                isLoading: false,
-                isSuccess: false,
-                isFailed: true,
-                error: payload.error
-            })
-        case DELETE_ARTICLE_REQUEST:
-            return Object.assign({}, state, {
-                isLoading: true,
-                isSuccess: false,
-                isFailed: false,
-            })
-        case DELETE_ARTICLE_SUCCESS:
-            return Object.assign({}, state, {
-                isLoading: false,
-                isSuccess: true,
-                isFailed: false,
-            })
-        case DELETE_ARTICLE_FAILED:
-            return Object.assign({}, state, {
-                isLoading: false,
-                isSuccess: false,
-                isFailed: true,
-                error: payload.error
-            })
-        default:
-            return state;
-    }
+    error: null,
 }
+
+export default handleActions({
+    GET_ARTICLE_LIST_REQUEST: (state, { payload }) => {
+        return Object.assign({}, state, {
+            isLoading: true,
+            isSuccess: false,
+            isFailed: false,
+        })
+    },
+    GET_ARTICLE_LIST_SUCCESS: (state, { payload }) => {
+        return Object.assign({}, state, {
+            isLoading: false,
+            isSuccess: true,
+            isFailed: false,
+            list: [...payload.data]
+        })
+    },
+    GET_ARTICLE_LIST_FAILED: (state, { payload }) => {
+        return Object.assign({}, state, {
+            isLoading: false,
+            isSuccess: false,
+            isFailed: true,
+            error: payload.error
+        })
+    },
+    DELETE_ARTICLE_REQUEST: (state, { payload }) => {
+        return Object.assign({}, state, {
+            isLoading: true,
+            isSuccess: false,
+            isFailed: false,
+        })
+    },
+    DELETE_ARTICLE_SUCCESS: (state, { payload }) => {
+        return Object.assign({}, state, {
+            isLoading: false,
+            isSuccess: true,
+            isFailed: false,
+        })
+    },
+    DELETE_ARTICLE_FAILED: (state, { payload }) => {
+        return Object.assign({}, state, {
+            isLoading: false,
+            isSuccess: false,
+            isFailed: true,
+            error: payload.error
+        })
+    }
+}, initialState)
+
+// export default function articleListReducer(state = initialState, { type, payload }) {
+//     switch (type) {
+//         case GET_ARTICLE_LIST_REQUEST:
+//             return Object.assign({}, state, {
+//                 isLoading: true,
+//                 isSuccess: false,
+//                 isFailed: false,
+//             })
+//         case GET_ARTICLE_LIST_SUCCESS:
+//             return Object.assign({}, state, {
+//                 isLoading: false,
+//                 isSuccess: true,
+//                 isFailed: false,
+//                 list: [...payload.data]
+//             })
+//         case GET_ARTICLE_LIST_FAILED:
+//             return Object.assign({}, state, {
+//                 isLoading: false,
+//                 isSuccess: false,
+//                 isFailed: true,
+//                 error: payload.error
+//             })
+//         case DELETE_ARTICLE_REQUEST:
+//             return Object.assign({}, state, {
+//                 isLoading: true,
+//                 isSuccess: false,
+//                 isFailed: false,
+//             })
+//         case DELETE_ARTICLE_SUCCESS:
+//             return Object.assign({}, state, {
+//                 isLoading: false,
+//                 isSuccess: true,
+//                 isFailed: false,
+//             })
+//         case DELETE_ARTICLE_FAILED:
+//             return Object.assign({}, state, {
+//                 isLoading: false,
+//                 isSuccess: false,
+//                 isFailed: true,
+//                 error: payload.error
+//             })
+//         default:
+//             return state;
+//     }
+// }
